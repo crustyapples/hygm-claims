573e6281ff10f1e23ac2c91f1664a082dd106112
Advait Deshpande <advait.deshpande@live.com>
2022-01-01 18:14:36 +0800

    removed unused functions on ayatakabot

---
+++ bot.py
@@ -0,0 +1,311 @@
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler, CallbackContext
+from telegram import ReplyKeyboardMarkup, Update
+import logging
+from bot_data import claims_ul, claims_ul3, receipt_del
+import os
+import copy
+from datetime import datetime
+
+PORT = int(os.environ.get('PORT', 5000))
+
+TOKEN = ''
+
+updater = Updater(token=TOKEN, use_context=True)
+
+dispatcher = updater.dispatcher
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+CHOOSE_USER, TYPING_REPLY, UPLOAD_QN, CLAIM_CHOICE, CLAIM_PRICE, CLAIM_QTY, RECEIPT_PIC, CLAIM_REMARKS, CHOOSE_MODE= range(9)
+
+def start(update = Update, context = CallbackContext) -> int:
+    update.message.reply_text("I exist to serve the hey, you got mail! main committee, use /help to learn more about what i can do")
+    context.user_data['username'] = update.message.chat.username
+    print(context.user_data)
+    
+    return CHOOSE_MODE
+
+def helpy(update, context):
+    context.bot.send_message(chat_id=update.effective_chat.id, text="/caps: type /caps followed by whatever u want me to yell! \n\n /claims: to make claims for your project related expenses!")
+
+def caps(update, context):
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+def greentea(update, context):
+    update.message.reply_text('i like ayataka only pls')
+
+edit_qn = [['ADD','SUBTRACT']]
+
+upload_qn = [['Upload','Edit']]
+
+claim_upload_qn = [['Upload','Claim']]
+
+logs_options = [
+    ['Original_ENG', 'Original_TL'],
+    ['Original_CHI', 'Original_ML','Spare Umbrellas'],
+]
+
+user_selector = [['Advait','Jaslyn','Jiwon'],['Joanne','Triston','Weilin']]
+
+claim_but = [['Claim']]
+
+stop_but = [['Stop']]
+
+general_claims = [
+    ['Stamps', 'Transport', 'Domain'],
+    ['Stickers', 'Welfare','Stationery'],
+    ['Card Printing','Others'],
+]
+
+STOP_BUTTON = ReplyKeyboardMarkup(stop_but, one_time_keyboard=True)
+claim_upload_markup = ReplyKeyboardMarkup(claim_upload_qn, one_time_keyboard=True)
+general_claims_markup = ReplyKeyboardMarkup(general_claims, one_time_keyboard=True)
+claim_but_markup = ReplyKeyboardMarkup(claim_but, one_time_keyboard=True)
+upload_qn_markup = ReplyKeyboardMarkup(upload_qn, one_time_keyboard=True)
+edit_qn_button = ReplyKeyboardMarkup(edit_qn, one_time_keyboard=True)
+user_selector_markup = ReplyKeyboardMarkup(user_selector, one_time_keyboard=True)
+logs_buttons = ReplyKeyboardMarkup(logs_options, one_time_keyboard=True)
+
+ 
+def facts_to_str(facts):
+# returns user data in readable way
+    output_list = facts['user']
+    y = f"User: {output_list[0]} \n"
+    output_str = [y]
+
+    if facts['entry'] == 'claims':
+        for i in range(len(output_list)):
+            try:
+                m = f"{output_list[i+1][0]} : {output_list[i+1][2]} ({output_list[i+1][3]})"
+            except:
+                continue
+            output_str.append(m)
+        return "\n".join(output_str)
+
+    for i in range(len(output_list)):
+        try:
+            m = f"{output_list[i+1][0]} : {output_list[i+1][1]}"
+        except:
+            continue
+        output_str.append(m)
+    
+    return "\n".join(output_str)
+
+def claims(update: Update, context: CallbackContext) -> int:
+    context.user_data['entry'] = 'claims'
+    context.user_data['username'] = update.message.chat.username
+    username = context.user_data['username']
+    
+    update.message.reply_text(
+            "First, let me know which committee member you are!",
+            reply_markup=ReplyKeyboardMarkup(
+                [
+                    ['Advait','Jaslyn','Jiwon','Joanne'],
+                    ['Triston','Weilin','Sheena','Ke Wei'],
+                    ['Raynard','Max','Devan','Sofea'],
+                    ['Bryan','Aldrich','Hui Shan','Maira','Dylan']
+                ], one_time_keyboard=True
+        )
+    )
+
+    return CHOOSE_USER
+
+def user_choice(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+    print(text)
+    context.user_data['user'] = [text]
+
+    if context.user_data['entry'] == 'claims':
+        update.message.reply_text('Press "Claim" to make your claim! ', reply_markup=claim_but_markup)
+            
+        return CLAIM_CHOICE   
+
+def claim_choice(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+    print(text)
+    update.message.reply_text("What are you submitting this claim for?", reply_markup=general_claims_markup)
+
+    return CLAIM_PRICE
+
+def claim_price(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+    print(text)
+    context.user_data['item'] = text
+    update.message.reply_text(
+        f"What is the price (in SGD) of one unit of {text}? \n\nAlternatively, you can make a bulk claim and \
+        indicate your quantity as 1 (e.g. if you chose 'Stationery' which may include multiple pens or glue-tape,\
+        you may submit as one item)"
+    )
+
+    return CLAIM_QTY
+
+def claim_qty(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+    print(text)
+    context.user_data['price'] = text
+    update.message.reply_text(f"How many of {context.user_data['item']} at SGD {text} per pc?")
+
+    return RECEIPT_PIC
+
+def receipt_ul(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+    print(text)
+    context.user_data['claim_qty'] = text
+
+    update.message.reply_text(f"Please send me your receipt(s) to claim {text} pcs of {context.user_data['item']} at SGD {context.user_data['price']} in one image!")
+
+    return CLAIM_REMARKS
+
+def claim_remarks(update: Update, context: CallbackContext) -> int:
+    image = update.message.photo[-1].file_id
+    t = str(datetime.now())[:10]
+    print(image)
+    context.user_data['image'] = image
+    claimant = context.user_data['user'][0]
+    item = context.user_data['item']
+    price = context.user_data['price']
+    qty = context.user_data['claim_qty']
+    receipt_img = context.bot.get_file(image)
+    receipt_img.download(f'receipts/{t}-{claimant}-{item}-SGD{price}x{qty}.jpg')
+
+    update.message.reply_text(
+                f"Any remarks to your claim of {context.user_data['claim_qty']} pcs of {context.user_data['item']} at SGD {context.user_data['price']}?"
+                , reply_markup=ReplyKeyboardMarkup([['NIL']], one_time_keyboard=True
+            )
+        )
+
+    print(context.user_data)
+    print(facts_to_str(context.user_data))
+    
+    return TYPING_REPLY
+
+def received_information(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+    
+    if context.user_data['entry'] == 'claims':
+        print(text)
+        context.user_data['remarks']=text
+
+        user_data = context.user_data
+        item = user_data['item']
+        user_data['user'].append([item,user_data['claim_qty'],str(float(user_data['price']) * float(user_data['claim_qty'])), user_data['remarks'], user_data['image']])
+        print(user_data)
+
+        for x in ['item','claim_qty','price','remarks','image']:
+            if x in user_data.keys():
+                del user_data[x]
+
+        update.message.reply_text(
+            "Neat! Just so you know, this is what you already told me:"
+            f"\n{facts_to_str(user_data)} \n\nDo you want to Upload or make more claims?",
+            reply_markup=claim_upload_markup,
+        )
+
+        return UPLOAD_QN
+
+def data_ul(update: Update, context: CallbackContext) -> int:
+    text = update.message.text
+    print(text)
+    user_data = context.user_data
+    
+    if context.user_data['entry'] == 'claims':
+        if text == 'Upload':
+            claims_ul3()
+            receipt_del()
+            update.message.reply_text(f'Uploading the following claims to Finance Sheet (New) on Google Sheets: \n{facts_to_str(user_data)} \n\n'
+            'You may start a new entry by calling /claims again! \n\n'
+            'Or end this session with /stop')
+            print(user_data)
+            ul_data = copy.deepcopy(user_data)
+  
+            context.bot.sendMessage(chat_id='-432713338', text=f'@jiwoney a new claim (SGD) has appeared! \n\n{facts_to_str(user_data)}')
+            
+            try:
+                for x in range(len(user_data['user'])):
+                    image_id = user_data['user'][x+1][4]
+                    context.bot.sendPhoto(chat_id='-432713338', photo=f"{image_id}")
+                    user_data['user'][x+1].remove(image_id)
+            except:
+                pass
+            
+            print(user_data)
+            ul_data = copy.deepcopy(user_data)
+            claims_ul(ul_data)
+            print(ul_data)
+            print(user_data)
+            
+            user_data.clear()
+            
+            return CHOOSE_MODE
+
+        if text == 'Claim':
+            update.message.reply_text("What are you submitting this claim for?", reply_markup=general_claims_markup)
+            return CLAIM_PRICE
+        
+
+
+def stop(update: Update, context: CallbackContext) -> int:
+    update.message.reply_text("Thanks! Just say the magic word if you need my help again!")
+    return ConversationHandler.END
+
+def main():
+
+    greentea_handler = CommandHandler('greentea', greentea)
+
+    start_handler = CommandHandler('pokka', start, Filters.user(user_id=[
+# add authorized user ids here
+            ]
+        )
+    )   
+
+    help_handler = CommandHandler('help', helpy)
+   
+    caps_handler = CommandHandler('caps', caps)
+
+    conv_handler = ConversationHandler(
+        entry_points=[start_handler],
+        states={
+            CHOOSE_MODE: [
+                CommandHandler('claims', claims),caps_handler, help_handler, greentea_handler
+            ],
+            CHOOSE_USER: [
+                MessageHandler(Filters.regex('^(Advait|Jaslyn|Jiwon|Joanne|Triston|Weilin|Shophouse|Sheena|Ke Wei|Raynard|Max|Devan|Sofea|Hui Shan|Aldrich|Bryan|Dylan|Maira)$'), user_choice)
+            ],
+            TYPING_REPLY: [
+                MessageHandler(
+                    Filters.text & ~(Filters.command | Filters.regex('^Stop$')),
+                    received_information,
+                )
+            ],
+            UPLOAD_QN: [
+                MessageHandler(Filters.text, data_ul)
+            ],
+            CLAIM_CHOICE: [
+                MessageHandler(Filters.regex('^Claim$') & ~(Filters.command | Filters.regex('^Stop$')), claim_choice)
+            ],
+            CLAIM_PRICE: [
+                MessageHandler(Filters.regex('^(Stamps|Transport|Domain|Stickers|Welfare|Stationery|Card Printing|Others)$'), claim_price)
+            ],
+            CLAIM_QTY: [
+                MessageHandler(Filters.regex(r"^([-+]?\d*\.\d+|\d+)$") & ~(Filters.command | Filters.regex('^Stop$')), claim_qty)
+            ],
+            RECEIPT_PIC: [
+                MessageHandler(Filters.regex(r"^([-+]?\d*\.\d+|\d+)$") & ~(Filters.command | Filters.regex('^Stop$')), receipt_ul)
+            ],
+            CLAIM_REMARKS: [
+                MessageHandler(Filters.photo & ~(Filters.command | Filters.regex('^Done$')), claim_remarks)
+            ],
+        },
+        fallbacks=[MessageHandler(Filters.regex('^(Stop)$'), stop),CommandHandler('stop', stop)]
+    )
+
+    dispatcher.add_handler(conv_handler)
+
+    updater.start_webhook(listen="0.0.0.0",
+                          port=int(PORT),
+                          url_path=TOKEN)
+    updater.bot.setWebhook('https://ancient-hamlet-17787.herokuapp.com/' + TOKEN)
+
+if __name__ == '__main__':
+    main()
---
+++ bot_data.py
@@ -0,0 +1,191 @@
+from __future__ import print_function
+import pickle
+import os
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+import googleapiclient.http
+from datetime import datetime
+from pytz import timezone
+
+# If modifying these scopes, delete the file token.pickle.
+SHEET_SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
+DRIVE_SCOPES = ['https://www.googleapis.com/auth/drive']
+
+# The ID and range of a the spreadsheet.
+
+FINANCE_SPREADSHEET_ID = '1zx7cnRJSe3Jj9NpTKXj1Os4y93VRnCgyESc4UCgyOfs'
+CLAIMS_RANGE_NAME = 'Spending!A2:H2'
+
+TIMESTAMP = str(datetime.now().astimezone(timezone('Asia/Singapore')))[:16]
+
+def sheets_authenticator():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token2.pickle'):
+        with open('token2.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SHEET_SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token2.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+    
+    return creds
+
+def drive_authenticator():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', DRIVE_SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return creds
+
+def claims_ul(claimsdata):
+    print("running...")
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    
+    service = build('sheets', 'v4', credentials=sheets_authenticator())
+
+    sheet = service.spreadsheets()
+    
+    user = claimsdata['user'][0]
+    
+    claim_list = claimsdata['user']
+    claim_list.pop(0)
+    
+    print("for the excel sheet", claim_list)
+
+    for c in claim_list:
+        c.append(user)
+        x = c[3]
+        c.pop(3)
+        c.extend(['Not Uploaded','Not Done',x])
+        c.insert(0,TIMESTAMP[5:10].replace('-','/'))
+
+    print(claim_list)
+    values = claim_list
+    body = {
+        'values': values
+    }
+
+    result = sheet.values().append(spreadsheetId=FINANCE_SPREADSHEET_ID,
+                                range=CLAIMS_RANGE_NAME,
+                                valueInputOption='USER_ENTERED',
+                                body=body).execute()
+    
+    print(result)
+
+def claims_ul2(folder_id):
+
+    service = build('drive', 'v3', credentials=drive_authenticator())
+    
+    receipts_dir = os.listdir('receipts')
+    receipt_pics = []
+    
+    for item in receipts_dir:
+        receipt_pics.append(f'{item}')
+        print(receipt_pics)
+
+    for x in receipt_pics:
+        if x != '.keep':
+            file_metadata = {
+            'name': x,
+            'parents': [folder_id]
+            }
+            media = googleapiclient.http.MediaFileUpload(f'receipts/{x}',
+                                    mimetype='image/jpeg')
+            results = service.files().create(body=file_metadata,
+                                                media_body=media,
+                                                fields='id').execute()
+        
+    print('File ID: %s' % results.get('id'))
+
+def receipt_del():
+    receipts_dir = os.listdir('receipts')
+    receipt_pics = []
+    
+    for item in receipts_dir:
+        receipt_pics.append(f'{item}')
+        print(receipt_pics)
+
+    for y in receipt_pics:
+        path = os.path.join('receipts',y)
+        os.remove(path)
+
+def claims_ul3():
+    service = build('drive', 'v3', credentials=drive_authenticator()) 
+    months_list = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November','December']
+    claims_month = f'{TIMESTAMP[5:7]}/{TIMESTAMP[:4]} {months_list[int(TIMESTAMP[5:7])-1]} BOT'
+    claims_date = f'{TIMESTAMP[5:7]}/{TIMESTAMP[8:10]} BOT'
+    claims_bot_folder = '1P-nxakvDSuYzmMC3agf_nuhpN913RuFD'
+    month_folder_id = ''
+    date_folder_id = ''
+
+    folder_res = service.files().list(q = f"mimeType = 'application/vnd.google-apps.folder' and name = '{claims_month}' and trashed = false" ,spaces='drive', pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    date_res = service.files().list(q = f"mimeType = 'application/vnd.google-apps.folder' and name = '{claims_date}' and trashed = false" ,spaces='drive', pageSize=10, fields="nextPageToken, files(id, name)").execute()
+    
+    # check for folder & date existing
+    
+    print(folder_res.get('files', []))
+    print(date_res.get('files', []))
+
+    if len(folder_res.get('files', [])) >= 1 or len(date_res.get('files', [])) >= 1:
+        for x in folder_res.get('files', []):
+            month_folder_id = x.get('id')
+            print(f"\n\nfolders exist, uploading file...\nmonth folder name/id: {x.get('name')}/{month_folder_id}")
+        for y in date_res.get('files',[]):
+            date_folder_id = y.get('id')
+            print(f"\ndate folder name/id: {y.get('name')}/{date_folder_id}")
+
+    if len(folder_res.get('files', [])) == 0: 
+        # create folder by month if it doesnt exist
+        print("folders dont exist, creating...")
+        month_metadata = {
+            'name': claims_month,
+            'mimeType':'application/vnd.google-apps.folder',
+            'parents' : [claims_bot_folder]
+        }
+        
+        month_folder = service.files().create(body=month_metadata,fields='id').execute()
+        month_folder_id = month_folder.get('id')
+    
+    if len(date_res.get('files', [])) == 0:
+        # create folder by date if it doesnt exist
+        print("folders dont exist, creating...")
+        date_metadata = {
+        'name': claims_date,
+        'mimeType':'application/vnd.google-apps.folder',
+        'parents': [month_folder_id]
+        }
+
+        date_folder = service.files().create(body=date_metadata,fields='id').execute()
+        date_folder_id = date_folder.get('id')
+
+    claims_ul2(date_folder_id)
+
